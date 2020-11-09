dcc1bd2753592f6a2270398cd1f1c10c7ad796fa
Alexander A. Klimov <alexander.klimov@icinga.com>
2020-11-09 13:03:50 +0100

    Revert "Drone CI: don't cache"
    This reverts commit 2bed7df3f1b86fb499d600fb3ca3924ee350e86a.

--- .drone/deb/binary.sh
+++ .drone/deb/binary.sh
@@ -2,6 +2,8 @@
 set -exo pipefail
 
 . .drone/setup.sh
+. .drone/ccache.sh
 
 cd deb-icinga2
 icinga-build-deb-binary
+ccache -s
--- .drone/deb/source.sh
+++ .drone/deb/source.sh
@@ -4,5 +4,7 @@ set -exo pipefail
 . .drone/setup.sh
 
 git clone https://git.icinga.com/packaging/deb-icinga2.git
+
 cd deb-icinga2
+ln -vs ../ccache .
 icinga-build-deb-source
